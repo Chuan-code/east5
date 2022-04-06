c557d7118d4f517dec92bae59e72fa47afd5aa93
CuzInvestigator <72342706+Chuan-code@users.noreply.github.com>
2022-04-06 11:08:52 +0800

    initial setting

--- test2.sql
+++ test2.sql
@@ -1,3 +1,3 @@
 drop table if exists fmbank_east5.east5_test2;
 create table if not exists fmbank_east5.east5_test2 as
-    select t.表名 from fmbank_east5.east5_test1 t
+    select t1.表名 from fmbank_east5.east5_test1 t1
